715437239d2b2e598ee30e7ba12d34f6dd1686f9
NawaNawa <ltue2129@gmail.com>
2018-04-20 21:44:47 +0800

    新增 page存於dataset

--- js/main.js
+++ js/main.js
@@ -102,6 +102,48 @@ NawaNawa.buttonEnableChecker=function ()
             theOtherButton.classList.remove("disable");
     }
 }
+/**
+ * 尋找某元素的祖先
+ *@argument oneQuery只包含#id 或 .className 或 tagname的單組選擇
+ */
+NawaNawa.ancestorSelector=function(oneQuery=".article",htmlElement)
+{
+    if(!htmlElement)
+    {
+        console.log("請填入htmlElement於第二個參數作為基準");
+        return;
+    }
+    let type=oneQuery[0];
+    let content="";
+    htmlElement=htmlElement.parentElement;
+    if(type=="."||type=="#")
+         content=oneQuery.slice(1,oneQuery.length);
+    else
+   {
+        content=oneQuery;
+        type="";
+    }
+    if(type==".")//class
+    { 
+        for(;htmlElement!=document.body;htmlElement=htmlElement.parentElement)
+            if(htmlElement.classList.contains(content))
+                return htmlElement;
+    }
+    else if(type=="#")//id
+    {
+        for(;htmlElement!=document.body;htmlElement=htmlElement.parentElement)
+            if(htmlElement.id==content)
+                return htmlElement;
+    }   
+    else if(type=="")//tag
+    {
+        for(;htmlElement!=document.body;htmlElement=htmlElement.parentElement)
+            if(htmlElement.tagName==content.toUpperCase())
+                return htmlElement;
+    }
+    return undefined;
+
+}
 NawaNawa.nextPageListener=
 function(e)
 {
@@ -111,6 +153,8 @@ function(e)
     let theOtherButton=this.parentElement.querySelector(".lastPageButton");
     let content=pages.querySelector(".pageContent");
     let button=this;
+    let article=NawaNawa.ancestorSelector(".article",this);
+    article.dataset.page++;
     $(pages).hide().load(content.dataset.nextpagesrc,function(){
         let content=this.querySelector(".pageContent");
         if(content)
@@ -120,7 +164,7 @@ function(e)
                     button.classList.add("disable");
             if(content.dataset.lastpagesrc)
                 if(theOtherButton.classList.contains("disable"))
-                theOtherButton.classList.remove("disable");
+                    theOtherButton.classList.remove("disable");
             $(pages).fadeIn('500');
         }
     });
@@ -130,6 +174,8 @@ function(e)
 {
     if(this.classList.contains("disable"))
         return;
+    let article=NawaNawa.ancestorSelector(".article",this);
+        article.dataset.page--;
     let pages=this.parentElement.querySelector(".pages");
     let theOtherButton=this.parentElement.querySelector(".nextPageButton");
     let content=pages.querySelector(".pageContent");
@@ -144,7 +190,7 @@ function(e)
             if(content.dataset.nextpagesrc)
                 if(theOtherButton.classList.contains("disable"))
                 theOtherButton.classList.remove("disable");
-                $(pages).fadeIn('500');
+            $(pages).fadeIn('500');
         }
     })
 }
@@ -161,6 +207,7 @@ $(".nextPageButton").on("click",NawaNawa.nextPageListener);
 $(".lastPageButton").on("click",NawaNawa.lastPageListener);
 $(".playNow").on("click",()=>{window.open("https://nawanae.github.io/PokemonYellow/start.html","_blank")})
 $(".gitHub").on("click",()=>{window.open("https://github.com/NawaNae/PokemonYellow","_blank")})
+let inmagePreLoad=[(new Image()).src="image/directions.jpg",(new Image()).src="image/AB.jpg",(new Image()).src="image/select.jpg"];
 })();
 window.onhashchange=
 function(e)
